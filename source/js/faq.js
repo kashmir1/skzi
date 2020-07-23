4696c492ec9fee87a3ca909ac5de74b60106ba03
kashmir1 <53399927+kashmir1@users.noreply.github.com>
2020-07-23 17:58:57 +1000

    пишет скрипт faq

---
+++ source/js/faq.js
@@ -0,0 +1,21 @@
+let answers = document.querySelectorAll('.faq__answer');
+let questions = document.querySelectorAll('.faq__question');
+
+
+answers.forEach(function(el) {
+  el.style.display = 'none';
+});
+
+questions.forEach(function(el) {
+  el.onclick = () => {
+    if (el.nextSibling.nextSibling.style.display === 'none') {
+      el.nextSibling.nextSibling.style.display = 'block';
+      el.classList.remove('faq__close');
+      el.classList.add('faq__open', 'faq__question--open');
+    } else {
+      el.nextSibling.nextSibling.style.display = 'none';
+      el.classList.add('faq__close');
+      el.classList.remove('faq__open', 'faq__question--open');
+    }
+  };
+});
