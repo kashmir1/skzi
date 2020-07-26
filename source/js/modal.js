ada302081b784c50da2ae045b2c64b370837a524
kashmir1 <53399927+kashmir1@users.noreply.github.com>
2020-07-27 09:44:24 +1000

    WIP

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,14 +1,21 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
+var modal2 = document.getElementById("myModal-2");
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn-2");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close-2")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function(evt) {
+  evt.preventDefault();
+  modal.style.display = "block";
+};
+
+btn2.onclick = function(evt) {
   evt.preventDefault();
   modal.style.display = "block";
 };
@@ -19,9 +26,20 @@ span.onclick = function(evt) {
   modal.style.display = "none";
 };
 
+span2.onclick = function(evt) {
+  evt.preventDefault();
+  modal.style.display = "none";
+};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
     modal.style.display = "none";
   }
 };
